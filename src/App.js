8ff1295964e40148403124558bd35dc6a96f37e5
Ramon Vitor <vitorramon@hotmail.com>
2021-08-02 21:13:22 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dogPicture: '',
+      loading: true,
+    };
+
+    this.fetchRandomDog = this.fetchRandomDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchRandomDog();
+  }
+
+  async fetchRandomDog() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const fetchURL = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await fetchURL.json();
+        this.setState({
+          loading: false,
+          dogPicture: data.message,
+        });
+      },
+    );
+  }
+
+  render() {
+    const { dogPicture, loading } = this.state;
+    const loadingElement = <span>Loading...</span>;
+    const imgDog = <img src={ dogPicture } alt={ dogPicture } />;
+
+    return (
+      <main>
+        <div>
+          { loading ? loadingElement : imgDog }
+        </div>
+        <button type="submit" onClick={ this.fetchRandomDog }>Próxima imagem</button>
+      </main>
+    );
+  }
 }
 
 export default App;
